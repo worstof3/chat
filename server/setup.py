1d9fcb00e5839461c02bdcdcf7bc6e4c62369a1e
Łukasz <33602077+worstof3@users.noreply.github.com>
2018-04-25 12:54:56 +0200

    Update setup.py

--- server/setup.py
+++ server/setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
-
 setup(
     name='server',
     version='1.0',
+    description='Simple chat server written with Python asyncio.',
+    url='https://github.com/worstof3/chat',
+    author='Łukasz Karpiński',
+    packages=['server'],
     test_suite='server.tests',
     entry_points={
         'console_scripts': [
-            'chatserver = server.script:main'
+            'chatserver=server.script:main'
         ]
     }
 )
