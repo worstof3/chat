422cb754283283c51a15885b257e8f1a0469b28f
Łukasz <33602077+worstof3@users.noreply.github.com>
2018-04-25 12:54:09 +0200

    Update setup.py

--- client/setup.py
+++ client/setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
-
 setup(
     name='client',
     version='1.0',
+    description='Simple chat client written with Python asyncio.',
+    url='https://github.com/worstof3/chat/client',
+    author='Łukasz Karpiński',
+    packages=['client'],
     test_suite='client.tests',
     entry_points={
         'console_scripts': [
-            'chatclient = client.script:main'
+            'chatclient=client.script:main'
         ]
     }
 )
